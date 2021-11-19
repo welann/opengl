05e5b9c57fa9a57ba1a48830d71f0c663a855073
welann <wzcing@outlook.com>
2021-11-19 22:54:08 +0800

    ♥ 2.2 基础光照 增加了镜面反射

--- res/colors.fs
+++ res/colors.fs
@@ -7,6 +7,7 @@ in vec3 FragPos;
 uniform vec3 objectColor;
 uniform vec3 lightColor;
 uniform vec3 lightPos;
+uniform vec3 viewPos;
 
 void main()
 {
@@ -18,6 +19,15 @@ void main()
 //     FragColor = vec4(lightColor * objectColor, 1.0);
     float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * lightColor;
-    vec3 result = (ambient + diffuse) * objectColor;
+
+    float specularStrength = 0.9;
+
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    vec3 result = (ambient + diffuse + specular) * objectColor;
     FragColor = vec4(result, 1.0);
 }
