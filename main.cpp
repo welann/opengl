cd6101c57f311897550808585f2600322ef9bfe0
welann <wzcing@outlook.com>
2021-11-20 17:52:53 +0800

    ♥ 2.5 投光物 聚光源

--- main.cpp
+++ main.cpp
@@ -242,6 +242,11 @@ int main() {
         lightingShader.setFloat("light.linear", 0.09f);
         lightingShader.setFloat("light.quadratic", 0.032f);
 
+        lightingShader.setVec3("light.position", camera.Position);
+        lightingShader.setVec3("light.direction", camera.Front);
+        lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+        lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
+
         // material properties
         lightingShader.setFloat("material.shininess", 32.0f);
 
